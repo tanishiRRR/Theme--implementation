09dfbff7190b749017fa857b5dcd1b599ff7ee00
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-03-26 13:23:05 +0000

    顧客用モデルと管理者用モデルを作成

--- db/migrate/20230326130827_create_admins.rb
+++ db/migrate/20230326130827_create_admins.rb
@@ -1,9 +1,6 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-      
-      t.string :email, null: false # メールアドレス
-      t.string :encrypted_password, null: false  # パスワード
 
       t.timestamps
     end
