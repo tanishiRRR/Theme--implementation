ea07a0ea77fb7d8d2007f8cf21abb9a3143583e5
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-03-27 15:55:07 +0000

    コントローラー修正(top,about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
 
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
   namespace :public do
+    root to: 'homes/top'
+    get 'homes/about' => 'homes#about',as: 'about'
+  end
+
+    namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
     get 'addresses/create'
@@ -29,21 +41,13 @@ Rails.application.routes.draw do
     get 'customers/unsubscribe'
     get 'customers/withdraw'
   end
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-root to: 'homes#top'
-get 'homes/about' => 'homes#about',as: 'about'
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
