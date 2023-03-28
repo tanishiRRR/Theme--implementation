d296180a0bd2488add3b7cc5ab4ef27dcd1f699f
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-03-28 10:54:42 +0000

    コントローラー修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # ログイン機能
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 画像投稿機能
+  has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,40 +7,33 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
-  namespace :public do
-    root to: 'homes/top'
-    get 'homes/about' => 'homes#about',as: 'about'
-  end
-
-    namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/comfirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about',as: 'about'
+  get 'items' => 'public/items#index'
+  get 'items/:id' => 'public/items#show'
+  
+  
+  get 'addresses/index'
+  get 'addresses/edit'
+  get 'addresses/create'
+  get 'addresses/update'
+  get 'addresses/destroy'
+  get 'orders/new'
+  get 'orders/comfirm'
+  get 'orders/complete'
+  get 'orders/create'
+  get 'orders/index'
+  get 'orders/show'
+  get 'cart_items/index'
+  get 'cart_items/update'
+  get 'cart_items/destroy'
+  get 'cart_items/destroy_all'
+  get 'cart_items/create'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/update'
+  get 'customers/unsubscribe'
+  get 'customers/withdraw'
 
 # 管理者用
 # URL /admin/sign_in ...
@@ -48,38 +41,25 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
-
   namespace :admin do
+    get 'homes/top'
     get 'order_details/update'
-  end
-  namespace :admin do
     get 'orders/show'
     get 'orders/update'
-  end
-  namespace :admin do
     get 'customers/index'
     get 'customers/show'
     get 'customers/edit'
     get 'customers/update'
-  end
-  namespace :admin do
     get 'genres/index'
     get 'genres/create'
     get 'genres/edit'
     get 'genres/update'
-  end
-  namespace :admin do
     get 'items/index'
     get 'items/new'
     get 'items/create'
     get 'items/show'
     get 'items/edit'
     get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
     get 'sessions/new'
     get 'sessions/create'
     get 'sessions/destroy'
