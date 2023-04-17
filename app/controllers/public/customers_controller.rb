1b82b2ad6e70d78d9afd1912715dcccffaeaf6b1
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-04-17 15:53:55 +0000

    登録情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,14 +1,25 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+  before_action :customer_scan, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+
 
   def show
-    @customer = Customer.find(current_customer.id)
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    if @customer.save
+      flash[:notice] = "You have updated customer successfully."
+      redirect_to customers_my_page_path
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
@@ -18,10 +29,15 @@ class Public::CustomersController < ApplicationController
   end
 
   private
-    def is_matching_login_customer
-      customer_id = params[:id].to_i
-      unless customer_id == current_customer.id
-        redirect_to customers_my_page_path(current_customer.id)
+
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :emai, :postal_code, :address, :telephone_number)
+    end
+
+    def customer_scan
+      unless current_customer
+        redirect_to customers_my_page_path
       end
     end
+
 end
