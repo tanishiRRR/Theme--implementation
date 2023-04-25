fcfef5e5f08207c68d7dcabd1fa29c336d2b9a72
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-04-25 18:56:30 +0000

    顧客ごとの注文履一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,27 @@
 class Admin::OrdersController < ApplicationController
+  before_action :admin_scan, only: [:index, :show, :edit, :update]
+
   def show
+    @customer = Customer.find(params[:id])
+    @orders = Order.where(customer_id: params[:id]).order(created_at: :DESC).page(params[:page]).per(10)
   end
 
   def update
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:status)
+    end
+
+    def order_detail_params
+      params.require(:orderdetail).permit(:making_status)
+    end
+
+    def admin_scan
+      unless current_admin
+        redirect_to new_admin_session_path
+      end
+    end
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -6,7 +6,7 @@ class OrderDetail < ApplicationRecord
   # 商品
   belongs_to :item
 
-  enum status: { cannot_be_manufactured: 0, waiting_for_production: 1, in_production: 2, completion_of_production: 3 }
+  enum making_status: { cannot_be_manufactured: 0, waiting_for_production: 1, in_production: 2, completion_of_production: 3 }
 
   def subtotal
     price * amount
