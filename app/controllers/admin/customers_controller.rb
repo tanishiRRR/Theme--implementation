55d8589910ced44606db4434c6490e3aa39d6e2f
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-04-25 15:50:33 +0000

    Admin注文履歴一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,31 @@
 class Admin::CustomersController < ApplicationController
+  before_action :admin_scan, only: [:index, :show, :edit, :update]
+
   def index
+    @customers = Customer.all.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.save
+    flash[:notice] = "カート内商品を編集しました。"
+    redirect_to admin_customer_path
   end
+
+  private
+
+  def admin_scan
+    unless current_admin
+      redirect_to new_admin_session_path
+    end
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # ログイン後のリダイレクト先
-  # def after_sign_in_path_for(resource_or_scope)
-  #   if resource_or_scope == Admin
-  #     # byebug
-  #     admin_root_path
-  #   else
-  #     customers_my_page_path
-  #   end
-  # end
-
-   # ログアウト後のリダイレクト先
-  # def after_sign_out_path_for(resource_or_scope)
-  #   if resource_or_scope == :admin
-  #     # byebug
-  #     new_admin_session_path
-  #   else
-  #     new_customer_session_path
-  #   end
-  # end
-
   protected
 
   def configure_permitted_parameters
